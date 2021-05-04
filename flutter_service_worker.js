5c8c0e431f51da3b330e274f4b7cc80eca2d6c20
PedroMGomes <pedrogomesprog@gmail.com>
2021-05-04 01:27:45 +0000

    Update gh-pages to output generated at d427053

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,8 +33,8 @@ const RESOURCES = {
 "assets/assets/icons/ice.svg": "f82b7e5d85efb43a4e5001ed490269d4",
 "assets/assets/icons/normal.svg": "8f2a7aaa9ae7bd49980f1b2b99a2577f",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"index.html": "6d5d231d58cc900dcdd76866435c3acf",
-"/": "6d5d231d58cc900dcdd76866435c3acf"
+"index.html": "a05927743b9bb458f1670f43c0815528",
+"/": "a05927743b9bb458f1670f43c0815528"
 };
 
 // The application shell files that are downloaded before a service worker can
